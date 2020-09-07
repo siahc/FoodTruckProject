b1dfefb0db259f0459e4db28f25f39cf40e2ffd7
Josiah Candler <josiahcandler@Josiahs-MacBook-Pro.local>
2020-09-07 13:24:19 -0600

    Updated inserting helpful comments

--- src/com/skilldistillery/foodproject/FoodTruck.java
+++ src/com/skilldistillery/foodproject/FoodTruck.java
@@ -6,8 +6,11 @@ public class FoodTruck {
 	private String foodType;
 	private double rating;
 	private int id;
+	// nextTruckID is incremented on every object creation by the constructor.
+	// the nextTruckId is for any food truck created and is only referenced during creation.
 	private static int nextTruckId = 1;
 
+	// 
 	public FoodTruck(String n, String ft, double r) {
 		this.name = n;
 		this.foodType = ft;
--- src/com/skilldistillery/foodproject/FoodTruckApp.java
+++ src/com/skilldistillery/foodproject/FoodTruckApp.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class FoodTruckApp {
 	private FoodTruck[] list;
 	Scanner scan = new Scanner(System.in);
-	
+
 	public static void main(String[] args) {
 		FoodTruckApp fta = new FoodTruckApp();
 		fta.run();
@@ -18,52 +18,52 @@ public class FoodTruckApp {
 		getTruckListFromUser();
 //		getTruckListForTesting();
 
-	
 		// show menu to user with the following items:
 		//
 		// 1 List all existing food trucks.
 		// 2 Show average rating of food trucks.
 		// 3 Show highest-rated food truck.
 		// 4 Quit.
-		while ( true ) {
-			boolean quit = false;
-			
+		boolean quit = false;
+		while (!quit) {
+
 			System.out.println();
-			System.out.println( "Press 1 for list of food trucks:");
+			System.out.println("Press 1 for list of food trucks:");
 			System.out.println("Press 2 for average rating of food trucks");
 			System.out.println("Press 3 for the highest rated food truck");
 			System.out.println("Press 4 to quit");
 			System.out.println();
-			
+
+			// read the user selection
 			System.out.println("Input a menu option:");
 			int menuItem = scan.nextInt();
-			
-			// read keyboard
-			switch (menuItem)
-			{
-			case 1: {
-				listFoodTrucks();
-				break;
+
+			// switch based on user input and can break out of switch
+			// (Switch is built out of case statements- aka a fork in the road
+			switch (menuItem) {
+				case 1: {
+					listFoodTrucks();
+					break;
+				}
+				case 2: {
+					showAverageRatings();
+					break;
+				}
+				case 3: {
+					showHighestRatedFoodTruck();
+					break;
+				}
+				case 4: {
+					// if user selects quit, program will exit the loop
+					quit = true;
+					break;
+				}
+				default: {
+					// print bad input
+					System.out.println("Please select a valid number");
+				}
 			}
-			case 2: {
-				showAverageRatings();
-				break;
-			}
-			case 3: {
-				showHighestRatedFoodTruck();
-				break;
-			}
-			case 4: {
-				quit = true;
-				break;
-			}
-			default: {
-				// print bad selection
-			}
-			}
-			if ( quit == true ) {
-				break;
-			}
+
 		}
 
 		System.out.println("Goodbye :)");
@@ -71,6 +71,9 @@ public class FoodTruckApp {
 
 	private void getTruckListFromUser() {
 		// get initial input from user
+		// iterating/progressing through list of food trucks.
+		// starts at 0 and runs till it is no longer less than length.
+		// iteration operator is ++
 		for (int i = 0; i < list.length; i++) {
 			System.out.println("Please enter the name of the food truck or 'quit': ");
 			String name = scan.next();
@@ -87,7 +90,8 @@ public class FoodTruckApp {
 			list[i] = f1;
 		}
 	}
-	
+
+	// this method is useful for development
 	private void getTruckListForTesting() {
 		list[0] = new FoodTruck("Tacos Super", "mexican", 4.2987654);
 		list[1] = new FoodTruck("The Hat", "pastrami", 4.5);
@@ -96,64 +100,52 @@ public class FoodTruckApp {
 
 	// prints all non-nil food trucks in list
 	private void listFoodTrucks() {
-		
+
 		// loop over all 5 possible trucks...
 		for (int index = 0; index < list.length; index++) {
-			if ( list[index] != null) {
+			if (list[index] != null) {
 				printFoodTruck(list[index]);
 			}
 		}
 
 	}
-
+	
+	// This displays the list nicely
 	private void printFoodTruck(FoodTruck truck) {
 		System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
 		System.out.println(truck);
 		System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
 	}
-	
+
 	private void showAverageRatings() {
-		
+
 		// loop over all 5 possible trucks...
 		int trucksFound = 0;
-		double TotalRatings= 0.0 ;
-		
-		for (int index = 0; index < list.length; index++)
-		{
-			if ( list[index] != null) {
+		double TotalRatings = 0.0;
+
+		for (int index = 0; index < list.length; index++) {
+			if (list[index] != null) {
 				trucksFound++;
 				// Calculate the average
 				TotalRatings += list[index].getRating();
-			} else {
-				// We hit null so there must be no more trucks!
-				break;
 			}
 		}
-		
+		// went through list, added up total trucks found and total ratings
+		// then displayed results to user including calculated average rating (only result)
 		double averageRating = (TotalRatings / trucksFound);
 		System.out.println("The average rating is: " + averageRating);
-		/*
-		 * { double[] rating = { 1, 2, 3, 4, 5 }; double min = rating[0];
-		 * 
-		 * for (int r = 1; r =< rating.length; r++) { if (max > rating[r]) { min =
-		 * rating[r]; } if (max < rating[r]) { max = rating[r]; } }
-		 * 
-		 * System.out.println("Min: " + min);
-		 * 
-		 * }
-		 */
+		
 	}
-	
+
 	private void showHighestRatedFoodTruck() {
 		// loop over all 5 possible trucks...
-		double highestRated = 0.0 ;
+		double highestRated = 0.0;
 		FoodTruck highestFoundTruck = null;
-		
-		for (int index = 0; index < list.length; index++)
-		{
-			if ( list[index] != null) {
+
+		for (int index = 0; index < list.length; index++) {
+			if (list[index] != null) {
 				// Calculate the average
-				if ( list[index].getRating() > highestRated ) {
+				if (list[index].getRating() > highestRated) {
 					highestRated = list[index].getRating();
 					highestFoundTruck = list[index];
 				}
@@ -162,7 +154,7 @@ public class FoodTruckApp {
 				break;
 			}
 		}
-		if ( highestFoundTruck != null ) {
+		if (highestFoundTruck != null) {
 			printFoodTruck(highestFoundTruck);
 		}
 	}
